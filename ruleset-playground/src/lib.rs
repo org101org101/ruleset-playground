49187dae4977268bdb2bd7ddccf2022ad9fa430e
deantheking <117676136+deantheking@users.noreply.github.com>
2026-02-06 03:44:43 +0000

    update

--- ruleset-playground/src/lib.rs
+++ ruleset-playground/src/lib.rs
@@ -1,12 +1,3 @@
 pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = ruleset_playground_derive::add(2, 2);
-        assert_eq!(result, 4);
-    }
+    ruleset_playground_derive::add(left, right)
 }
--- ruleset-playground/src/main.rs
+++
@@ -1,4 +0,0 @@
-
-fn main() {
-    println!("{}", ruleset_playground_derive::add(1,1));
-}
