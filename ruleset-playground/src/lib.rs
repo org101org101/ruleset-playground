51b88a6c33f47abe8e538ea7479032740a4aa8d1
deantheking <117676136+deantheking@users.noreply.github.com>
2026-02-06 03:28:11 +0000

    add derive to main lib

--- ruleset-playground/src/lib.rs
+++ ruleset-playground/src/lib.rs
@@ -4,11 +4,9 @@ pub fn add(left: u64, right: u64) -> u64 {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn it_works() {
-        let result = add(2, 2);
+        let result = ruleset_playground_derive::add(2, 2);
         assert_eq!(result, 4);
     }
 }
